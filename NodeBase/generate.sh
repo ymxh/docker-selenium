16671aa4fa4c7d3749d022ba49d463fb309ba807
ymxhtest <ymxhtest@163.com>
2018-06-14 18:45:56 +0800

    nodebase dockerfile

---
+++ NodeBase/generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION=$1
+
+echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
+echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
+echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
+echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
+echo FROM ymxhtest/ds-base:$VERSION >> ./Dockerfile
+cat ./Dockerfile.txt >> ./Dockerfile
--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 （3）下载selenium-server-standalone-2.53.1.jar，存放在/opt/selenium/selenium-server-standalone.jar<br>
 ## 2.hub
 添加hub DockerFile，主要基于base:2.53.1<br>
-（1）监听端口为4444
+（1）监听端口为4444<br>
+## 3.NodeBase 基于base:2.53.1,准备node节点
