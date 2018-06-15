753a97ca3bb28a57c18427a829ca32e5d1e17966
ymxhtest <ymxhtest@163.com>
2018-06-15 11:52:04 +0800

    node chrome dockerfile

---
+++ NodeChrome/intsallchrome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CHROME_VERSION=65.0.3325.181
+
+#============================================                                                                                                     
+# 下载 安装 Google Chrome                                                                                                                           
+#============================================                                                                                                     
+sudo wget -q http://www.slimjetbrowser.com/chrome/lnx/chrome64_${CHROME_VERSION}.deb
+
+# 更新一次
+sudo apt-get update -y
+sudo dpkg -i chrome64_${CHROME_VERSION}.deb  >/dev/null  2>&1
+# 修复
+sudo apt-get -f install -qqy
+sudo dpkg -i chrome64_${CHROME_VERSION}.deb
