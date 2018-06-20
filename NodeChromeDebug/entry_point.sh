f17c6049576f5f19966acb7ade38875ba832436a
ymxhtest <ymxhtest@163.com>
2018-06-20 16:41:08 +0800

    update

--- NodeChromeDebug/entry_point.sh
+++ NodeChromeDebug/entry_point.sh
@@ -5,27 +5,13 @@ source /opt/bin/functions.sh
 
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
-# -e 判断一个文件是否存在，这里当文件不存在返回false取反得到ture，执行then
 if [ ! -e /opt/selenium/config.json ]; then
   echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
   exit 1
 fi
 
-# In the long term the idea is to remove $HUB_PORT_4444_TCP_ADDR and $HUB_PORT_4444_TCP_PORT and only work with
-# $HUB_HOST and $HUB_PORT
-if [ ! -z "$HUB_HOST" ]; then
-  HUB_PORT_PARAM=4444
-  if [ ! -z "$HUB_PORT" ]; then
-      HUB_PORT_PARAM=${HUB_PORT}
-  fi
-  echo "Connecting to the Hub using the host ${HUB_HOST} and port ${HUB_PORT_PARAM}"
-  HUB_PORT_4444_TCP_ADDR=${HUB_HOST}
-  HUB_PORT_4444_TCP_PORT=${HUB_PORT_PARAM}
-fi
-
-# -z 判空；如果$HUB_PORT_4444_TCP_ADDR值为空，执行then
 if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
-  echo "Not linked with a running Hub container" 1>&2
+  echo Not linked with a running Hub container 1>&2
   exit 1
 fi
 
@@ -34,28 +20,47 @@ function shutdown {
   wait $NODE_PID
 }
 
-REMOTE_HOST_PARAM=""
 if [ ! -z "$REMOTE_HOST" ]; then
-  echo "REMOTE_HOST variable is set, appending -remoteHost"
-  REMOTE_HOST_PARAM="-remoteHost $REMOTE_HOST"
+  >&2 echo "REMOTE_HOST variable is *DEPRECATED* in these docker containers.  Please use SE_OPTS=\"-host <host> -port <port>\" instead!"
+  exit 1
 fi
 
 if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
+# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
+
 SERVERNUM=$(get_server_num)
 
 rm -f /tmp/.X*lock
 
-xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+env | cut -f 1 -d "=" | sort > asroot
+sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
+sudo -E -i -u seluser \
+  "$(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done)" \
+  DISPLAY=$DISPLAY \
+  xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
     -role node \
     -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
-    ${REMOTE_HOST_PARAM} \
     -nodeConfig /opt/selenium/config.json \
     ${SE_OPTS} &
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
+for i in $(seq 1 10)
+do
+  xdpyinfo -display $DISPLAY >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo Waiting xvfb...
+  sleep 0.5
+done
+
+fluxbox -display $DISPLAY &
+
+x11vnc -forever -usepw -shared -rfbport 5900 -display $DISPLAY &
+
 wait $NODE_PID
