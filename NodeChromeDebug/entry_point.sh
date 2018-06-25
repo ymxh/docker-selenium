54ddf59fcb3f1f47f4cb351f727a0fb8c815763f
ymxhtest <ymxhtest@163.com>
2018-06-25 18:52:37 +0800

    update dockerfile

--- NodeChromeDebug/entry_point.sh
+++ NodeChromeDebug/entry_point.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
 source /opt/bin/functions.sh
+echo "test1"
 /opt/selenium/generate_config > /opt/selenium/config.json
-
+echo "test2"
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
-
+echo "test3"
 if [ ! -e /opt/selenium/config.json ]; then
   echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
   exit 1
-fi
-
-
-# In the long term the idea is to remove $HUB_PORT_4444_TCP_ADDR and $HUB_PORT_4444_TCP_PORT and only work with
-# $HUB_HOST and $HUB_PORT
-if [ ! -z "$HUB_HOST" ]; then
-  HUB_PORT_PARAM=4444
-  if [ ! -z "$HUB_PORT" ]; then
-      HUB_PORT_PARAM=${HUB_PORT}
-  fi
-  echo "Connecting to the Hub using the host ${HUB_HOST} and port ${HUB_PORT_PARAM}"
-  HUB_PORT_4444_TCP_ADDR=${HUB_HOST}
-  HUB_PORT_4444_TCP_PORT=${HUB_PORT_PARAM}
 fi
 
 if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
@@ -33,10 +21,9 @@ function shutdown {
   wait $NODE_PID
 }
 
-REMOTE_HOST_PARAM=""
 if [ ! -z "$REMOTE_HOST" ]; then
-  echo "REMOTE_HOST variable is set, appending -remoteHost"
-  REMOTE_HOST_PARAM="-remoteHost $REMOTE_HOST"
+  >&2 echo "REMOTE_HOST variable is *DEPRECATED* in these docker containers.  Please use SE_OPTS=\"-host <host> -port <port>\" instead!"
+  exit 1
 fi
 
 if [ ! -z "$SE_OPTS" ]; then
@@ -58,7 +45,6 @@ sudo -E -i -u seluser \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
     -role node \
     -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
-    ${REMOTE_HOST_PARAM} \
     -nodeConfig /opt/selenium/config.json \
     ${SE_OPTS} &
 NODE_PID=$!
