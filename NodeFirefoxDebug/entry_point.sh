ee2c88317534e81a3e1b2cf047281f8f832d4f47
ymxhtest <ymxhtest@163.com>
2018-07-26 17:47:42 +0800

    update Node Debug

--- NodeFirefoxDebug/entry_point.sh
+++ NodeFirefoxDebug/entry_point.sh
@@ -1,41 +1,80 @@
 #!/bin/bash
 
-#这句比较重要，表示整个文件都以/bin/bash编译，这样linux才能明白文件的内容
+#source作用是立即编译生效
+source /opt/bin/functions.sh
+/opt/selenium/generate_config > /opt/selenium/config.json
 
-#定义ROOT、CONF变量
-ROOT=/opt/selenium
-CONF=$ROOT/config.json
+#export声明变量，并复制到子shell中
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
-#>表示创建config.json文件，并把generate_config内容写入
-$ROOT/generate_config >$CONF
+#判断文件不存在，则执行then
+if [ ! -e /opt/selenium/config.json ]; then
+  echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
+  exit 1
+fi
 
-#echo打印一句话，cat查看文件内容
-echo "starting selenium hub with configuration:"
-cat $CONF
+#判断变量为空，$HUB_PORT_4444_TCP_ADDR与$HUB_PORT_4444_TCP_PORT这两个变量在docker运行容器，通过--link参数来连接hub容器得到
+if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
+  echo Not linked with a running Hub container 1>&2
+  exit 1
+fi
 
-#-z判断为空，这里是判断$SE_OPTS这个变量不为空，则执行then，$SE_OPTS这个变量在docker启动容器时可传入，不传入则保持为空
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ ! -z "$REMOTE_HOST" ]; then
+  >&2 echo "REMOTE_HOST variable is *DEPRECATED* in these docker containers.  Please use SE_OPTS=\"-host <host> -port <port>\" instead!"
+  exit 1
+fi
+
 if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
-#定义一个shutdown函数，kill表示杀掉进程，wait等待kill任务执行完成
-function shutdown {
-    echo "shutting down hub.."
-    kill -s SIGTERM $NODE_PID
-    wait $NODE_PID
-    echo "shutdown complete"
-}
+# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
 
-#${JAVA_OPTS}在启动容器时传入，不传入保持为空；
-#启动hub节点，&表示同时执行把hub节点启动的进程id复制给NODE_PID变量
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
-  -role hub \
-  -hubConfig $CONF \
-  ${SE_OPTS} &
+#从functions.sh中得到
+SERVERNUM=$(get_server_num)
+
+rm -f /tmp/.X*lock
+#env得到root用户环境变量，cut截取sort排序保存在asroot文件， 
+#切换到seluser用户同理保存在asseluser文件，然后grep比较文件不同的值输出
+#xvfb运行node节点
+env | cut -f 1 -d "=" | sort > asroot
+  sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
+  sudo -E -i -u seluser \
+  $(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done) \
+  DISPLAY=$DISPLAY \
+  xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+    -role node \
+    -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
+    -nodeConfig /opt/selenium/config.json \
+    ${SE_OPTS} &
 NODE_PID=$!
 
-#捕捉SIGTERM、SIGINT信号会调用shutdown信号，执行kill hub节点进程操作
 trap shutdown SIGTERM SIGINT
+#循环10次，每次等待0.5秒，等待xvfb运行的信息完全放入黑洞
+#xdpyinfo显示xvfb运行的信息放入黑洞不显示出来
+for i in $(seq 1 10)
+do
+  xdpyinfo -display $DISPLAY >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo Waiting xvfb...
+  sleep 0.5
+done
 
-#注意NODE_PID存的是$!，表示的上一个子进程id，并不是存了一个id值，这里是等待上面的捕捉信号执行shutdown任务完成
+#通过fluxbox显示窗口
+fluxbox -display $DISPLAY &
+
+#启动远程服务，提供VNC客户端连接查看窗口
+#-forever关闭远程的vncviewer之后不关闭x11vnc的服务
+#-usepw 密码连接
+#-shared 可支持多个vncviewer客户端连接
+#-rfbport 监听端口
+x11vnc -forever -usepw -shared -rfbport 5900 -display $DISPLAY &
 wait $NODE_PID
